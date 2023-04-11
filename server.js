cbdf148d0f784575532db55a3f4c01245b72f2a8
Janinenw <janine.wexler@gmail.com>
2023-04-11 13:55:14 -0400

    do not merge yet, use cors added with frontend local host as origin

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-//import all available routes in our /routes/index.js the user can use
 require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
 const routes = require('./routes/index')
-const express=require('express');
 
 const app = express();
-// const mongoose = require('mongoose')
-// add this - import middlware
-// const cors = require("cors");
-const PORT=process.env.PORT
+const PORT = process.env.PORT;
 
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
-
-///////////////////////////////
-// MiddleWare
-////////////////////////////////
-// app.use(cors()); // to prevent cors errors, open access to all origins
 app.use(express.urlencoded({extended: true}))
-app.use(express.json()); // parse json bodies
+app.use(express.json());
 
-app.use('/', routes) 
-// app.get('/', (req, res)=>{
-//     res.send('hello world')
-// })
-//catch all 404 route! 
-app.use((req, res) => {res.status(404).json({message: "NOT A PROPER ROUTE"})})
+app.use('/', routes);
+
+app.use((req, res) => {
+  res.status(404).json({message: "NOT A PROPER ROUTE"})
+})
+
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
