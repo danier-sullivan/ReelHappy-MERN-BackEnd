f93bbe343cec06b62ec8b62e44453a49b7cafa6d
DanGit0893 <dannysullivan300@gmail.com>
2023-04-15 12:07:40 -0500

    adding Get route to trouble shoot deployment

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json()); // parse json bodies
 
 app.use('/', routes) 
-// app.get('/', (req, res)=>{
-//     res.send('hello world')
-// })
+app.get('/', (req, res)=>{
+    res.send('hello world')
+})
 //catch all 404 route! 
 app.use((req, res) => {res.status(404).json({message: "NOT A PROPER ROUTE"})})
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
